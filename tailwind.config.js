69ddb674be2a5891001f3646f0d50f50a9041529
CS-5 <me@carsonseese.com>
2022-03-23 11:23:46 -0400

    Fill out boiler plate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  mode: "jit",
+  darkMode: "class",
   theme: {
     extend: {},
   },
